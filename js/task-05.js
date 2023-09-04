f9414da9e4c8ba078a38c3abb68aadd26072a04b
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-04 16:52:04 +0300

    upd task05

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,6 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-    const name = event.currentTarget.value;
+    const name = event.currentTarget.value.trim();
     textOutput.textContent = name === "" ? "Anonymous" : name;
 });
