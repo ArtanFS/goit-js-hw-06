e4969e5afa140f52a0c9d63d23db0523a4c10ada
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-09-04 14:14:12 +0300

    make task09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const background = document.querySelector("body");
+const textColor = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+const handleClick = () => {
+  const bgColor = getRandomHexColor();
+  background.style.backgroundColor = bgColor;
+  textColor.textContent = bgColor;
+}
+
+btn.addEventListener("click", handleClick);
